00f54d88db08e330e7653e1e9b30f312607b29c9
env3d <jmadar@gmail.com>
2024-07-10 18:11:13 -0700

    adding devcontainer to repo

---
+++ .devcontainer/init.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install -y python3-pip
+sudo rm -r /var/www/html
+sudo ln -s $(pwd) /var/www/html
+sudo a2enmod cgid
+sudo service apache2 start
+pip install pytest
